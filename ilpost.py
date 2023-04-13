d06e51113e94dc7da0b8bc587d1c773ca2e76c8b
davideandreotti <davide1.andreotti@mail.polimi.it>
2023-04-13 22:46:34 +0200

    Prepared code for Bits and Flashes implementation

--- ilpost.py
+++ ilpost.py
@@ -1,3 +1,12 @@
+'''
+home post type-post category-italia 
+
+home post type-post category-bits
+
+archive arc-flashes closed flashes type-flashes tag-arnold-schwarzenegger selected current
+
+archive arc-flashes closed flashes type-flashes
+'''
 import requests
 from bs4 import BeautifulSoup
 import json
@@ -9,7 +18,12 @@ f.close()
 #token=''
 admin='25679064'
 chatid='-1001216458621'
-x = requests.get('https://ilpost.it')
+pages=['https://ilpost.it', 'https://www.ilpost.it/bits/', 'https://www.ilpost.it/flashes/']
+
+home_request = requests.get(pages[0])
+#bits_request=requests.get(pages[1])
+#flashes_request=requests.get(pages[2])
+
 def sendArticle(title, headline, link):
     method='/sendMessage'
     text='*'+title+'*\n\n_'+headline+'_\n'+link
@@ -18,27 +32,39 @@ def sendArticle(title, headline, link):
     r = requests.post(base+token+method, json=data, headers=headers)
     print(r.content)
 
+def contentProcessing(scan, page):
+    articles=dict()
+    for article in scan.find_all('article'):
+        if page in article['class']:
+            elements=dict()
+            id=article['id']
+            elements['id']=id
+            #print(id)
+            elements['img_url']=article.find('img')['src']
+            #print(img_url)
+            content=article.find("div", {"class": "entry-content"})
+            elements['title'] = content.h2.a['title']
+            elements['headline'] = content.p.a['title']
+            elements['link']=content.h2.a['href'].split('?')[0]
+            articles[id]=elements
+    return articles
+
+
 #reading latest scrape results
 f = open("latest_article.txt", "r")
 latest=json.loads(f.read())
 f.close()
 
 #scanning articles to retrieve information
-scan=BeautifulSoup(x.content.decode(encoding='utf-8'), 'html')
+#print(bits_request.headers)
+scan=BeautifulSoup(home_request.content.decode(encoding='utf-8'), features="html.parser")
+#scan_bits=BeautifulSoup(bits_request.content.decode(encoding='utf-8'), features="html.parser")
+#print(scan)
 articles=dict()
-for article in scan.find_all('article'):
-    if 'home' in article['class']:
-        elements=dict()
-        id=article['id']
-        elements['id']=id
-        #print(id)
-        elements['img_url']=article.find('img')['src']
-        #print(img_url)
-        content=article.find("div", {"class": "entry-content"})
-        elements['title'] = content.h2.a['title']
-        elements['headline'] = content.p.a['title']
-        elements['link']=content.h2.a['href'].split('?')[0]
-        articles[id]=elements
+articles=contentProcessing(scan, "home")
+#articles_bits=contentProcessing(scan_bits, "bits")
+#print(articles_bits)
+
 
 #merging old vs new scrape
 sorted_ids=list(articles.keys())
@@ -48,8 +74,8 @@ for id in sorted_ids:
     if id not in latest:
         elements=sorted_articles[id]
         #sending to telegram
-        #print(elements['title'], elements['headline'], elements['link'])
-        sendArticle(elements['title'], elements['headline'], elements['link'])
+        print(elements['title'], elements['headline'], elements['link'])
+        #sendArticle(elements['title'], elements['headline'], elements['link'])
 
 f = open("latest_article.txt", "w")
 f.write(json.dumps(sorted_ids))
